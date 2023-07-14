7670b61a5dfbf999157ae5c78cff8d701f98ff70
Andrew Platonov <andrew.platonov@protonmail.com>
2023-07-14 19:31:48 +0400

    Temp commit

--- src/main/java/io/jsight/JSight.java
+++ src/main/java/io/jsight/JSight.java
@@ -1,5 +1,21 @@
 package io.jsight;
 
+public class ErrorPosition() {
+    public String getFilepath();
+    public int getCol();
+    public int getIndex();
+    public int getLine();
+}
+
+public class ValidationError {
+    public int getCode();
+    public String getDetail();
+    public ErrorPosition getPosition();
+    public String getReportedBy();
+    public String getTitle();
+    public String getType();
+    public String ToJSON();
+}
 
 public class JSight {
     static {
@@ -9,8 +25,9 @@ public class JSight {
 
     public static native boolean Init();
     public static native String Stat();
-    public static native String ValidateHttpRequest(String schemaFilePath, String requestMethod, String requestUri, String requestPayload, String logFilePath);
-    public static native int ValidateHttpRequestTimeMks(String schemaFilePath, String requestMethod, String requestUri, String requestPayload, String logFilePath);
-    public static native String ValidateHttpResponse(String schemaFilePath, String requestMethod, String requestUri, int responseStatusCode, String responsePayload, String logFilePath);
-    public static native int ValidateHttpResponseTimeMks(String schemaFilePath, String requestMethod, String requestUri, int responseStatusCode, String responsePayload, String logFilePath);
+    public static native ClearCache();
+    
+    public static native ValidationError ValidateHttpRequest (String apiSpecFilePath, String requestMethod, String requestUri, requestHeaders, String requestBody);
+    public static native ValidationError ValidateHttpResponse(String apiSpecFilePath, String requestMethod, String requestUri, int responseStatusCode, String responseHeaders, String responseBody);
+    public static native String SerializeError(String format, error);
 }
